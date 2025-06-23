bf11fe0631b8f1e0f2b64d52fa07480bf287e646
Almasi <almasi89>
2025-06-23 10:10:47 +0300

    adds flask restful crud lab

--- server/app.py
+++ server/app.py
@@ -46,7 +46,25 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    def patch(self, id):
+        plant = Plant.query.get_or_404(id)
+        data = request.get_json()
 
+        if "is_in_stock" in data:
+            setattr(plant, "is_in_stock", data["is_in_stock"])
+        
+        db.session.commit()
+        return jsonify(plant.to_dict())  # Status 200 by default
+
+    def delete(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return {"error": "Plant not found"}, 404
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return "", 204
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
